f139f34aaf757c04e1cb25e0d09322945e231618
CasualElf34 <mael.guillaume.34@gmail.com>
2025-10-15 18:09:05 +0200

    ALL6

--- js/parcour.js
+++ js/parcour.js
@@ -1,7 +1,7 @@
 const ligne = document.getElementById('ligne-directrice');
 const walkingman = document.getElementById('walkingman');
 const etapes = document.querySelectorAll('.etape');
-const nbTirets = 80; // nombre de pointillés
+const nbTirets = 150; // nombre de pointillés
 let currentTiret = 0;
 let currentEtape = 0;
 
